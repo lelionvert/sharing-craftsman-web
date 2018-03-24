d3ca66dea7c8c30d8bf72f94fb4514415cbee50f
Cyril Pham-le <cyril.phamle@gmail.com>
2018-03-24 14:02:31 +0100

    update scripts

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -6,6 +6,7 @@ mkdir $1
 cp scripts/docker-compose.yml $1/docker-compose.yml
 cp scripts/Dockerfile $1/Dockerfile
 cp scripts/update_config.py $1/update_config.py
+cp scripts/web-infos.json $1/web-infos.json
 cp scripts/README.md $1/README.md
 cp -R dist $1/dist
 echo "End copying deployable files in app folder"
